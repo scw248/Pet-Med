7ea15326b51ce3dd65d514cf2914f9b1f5598a46
github username <github email address>
2019-04-08 15:16:42 -0500

    added login and signup views to application layout

--- PetID/config/routes.rb
+++ PetID/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root 'home#index'
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
