0b8cf00ce470d919b1a396a0d24fb0641ea5f2e1
github username <github email address>
2019-04-08 15:18:32 -0500

    Testing OmniAuth with regular sign-in

--- PetID/app/models/user.rb
+++ PetID/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
