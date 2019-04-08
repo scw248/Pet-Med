66c6cf06a40c7b3733c65bcaab31252a34623129
github username <github email address>
2019-04-08 14:09:36 -0500

    updated initializers, user model, and created new controller for OmniAuth

--- PetID/config/initializers/devise.rb
+++ PetID/config/initializers/devise.rb
@@ -296,4 +296,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.omniauth :facebook, "993474090855111
+  ", "377a455c831c7213fc66563eefcd6708"
 end
