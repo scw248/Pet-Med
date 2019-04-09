a227baf722dd430cd05b424ab6be6d4c276fe0b0
github username <github email address>
2019-04-08 19:25:26 -0500

    added nested resources in routes

--- PetID/config/routes.rb
+++ PetID/config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  resources :users, only: [:show] do
+    resources :pets, only: [:show]
+  end
+
+  resources :users, only: [:show] do
+    resources :appointments, only: [:show]
+  end
+
   resources :vet_providers
   resources :appointments
   resources :pets
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  
   # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 end
