f8a057d832443b421f17df24d69330d51c6f5a04
github username <github email address>
2019-04-09 14:03:00 -0500

    created users controller methods

--- PetID/app/controllers/users/users_controller.rb
+++ PetID/app/controllers/users/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
 
-  def show
-  end
-  
+  # def new
+  #   @user = User.new
+  # end
+
+  # def create
+  #   user = User.create(user_params)
+  #   session[:user_id] = user.id
+  #   redirect_to user_path(user)
+  # end
+
+  # def show
+  #   @user = user
+  # end
+
+  # private
+  #   def user_params
+  #     params.require(:user).permit(:email, :password)
+  #   end
+
+  #   def user
+  #     @user ||= User.find(params[:id])
+  #   end
 end
