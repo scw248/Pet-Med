b9f0edbadfc5026ee80f60a727ee8952959b0d6c
github username <github email address>
2019-04-09 14:08:10 -0500

    created pet controller methods

--- PetID/app/controllers/pets_controller.rb
+++ PetID/app/controllers/pets_controller.rb
@@ -1,3 +1,34 @@
 class PetsController < ApplicationController
+  def new
+    @pet = Pet.new
+  end
 
+  def create
+    new_pet = Pet.create(pet_params)
+    redirect_to pet_path(new_pet)
+  end
+
+  def show
+    @pet = pet
+  end
+
+  def edit
+    @pet = pet
+  end
+
+  def update
+    @pet = pet
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
+  private
+
+    def pet_params
+      params.require(:pet).permit(:name, :animal_type, :breed, :gender, :birthdate, :weight)
+    end
+
+    def pet
+      @pet ||= Pet.find(params[:id])
+    end
 end
