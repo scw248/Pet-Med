de2553fc8d787af96cc5a4624912937b868e1d3d
github username <github email address>
2019-04-10 15:29:15 -0500

    finalized appt partial and rendered in new and edit views

--- PetID/app/controllers/appointments_controller.rb
+++ PetID/app/controllers/appointments_controller.rb
@@ -17,6 +17,7 @@ class AppointmentsController < ApplicationController
   def create
     @appointment = Appointment.create(appointment_params)
     @appointment.user_id = current_user.id
+    # @appointment.save
     redirect_to appointment_path(@appointment)
   end
 
