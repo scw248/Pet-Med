55403e8f086b8960472c2b6dda09f4a6f6bdd8da
github username <github email address>
2019-04-10 14:07:27 -0500

    added partial form for pet, appts controller and views

--- PetID/app/controllers/appointments_controller.rb
+++ PetID/app/controllers/appointments_controller.rb
@@ -1,2 +1,37 @@
+# frozen_string_literal: true
+
 class AppointmentsController < ApplicationController
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    @appointment.user_id = current_user.id
+    redirect_to appointment_path(@appointment)
+  end
+
+  def show
+    @appointment = appointment
+  end
+
+  def edit
+    @appointment = appointment
+  end
+
+  def update
+    @appointment = appointment
+    @appointment.update(appointment_params)
+    redirect_to appointment_path(@appointment)
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:reason_for_visit, :date_time)
+  end
+
+  def appointment
+    @appointment ||= Appointment.find(params[:id])
+  end
 end
