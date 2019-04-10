3f7f726addb680a2c6f786e109fc27de6da712ba
github username <github email address>
2019-04-10 14:13:02 -0500

    added edit view to pets and updated show view

--- PetID/app/controllers/pets_controller.rb
+++ PetID/app/controllers/pets_controller.rb
@@ -8,6 +8,7 @@ class PetsController < ApplicationController
   def create
     @pet = Pet.create(pet_params)
     @pet.user_id = current_user.id
+    @pet.save
     redirect_to pet_path(@pet)
   end
 
