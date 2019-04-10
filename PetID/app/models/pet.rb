2be5ca8fbf027e3cdf8852f626d83768b7c5a112
github username <github email address>
2019-04-10 09:10:42 -0500

    Updating validations in pet model

--- PetID/app/models/pet.rb
+++ PetID/app/models/pet.rb
@@ -3,8 +3,11 @@
 class Pet < ApplicationRecord
   validates :name, presence: true
   validates :animal_type, presence: true
-  # validates :animal_type, inclusion: { in: types, presence: true } # can use helper
+  validates :animal_type, inclusion: { in: types, presence: true }
   validates :breed, presence: true
+  validates :breed, inclusion: { in: dog_breed, presence: true }, if: :type_dog
+  validates :breed, inclusion: { in: cat_breed, presence: true }, if: :type_cat
+  validates :breed, inclusion: { in: bunny_breed, presence: true }, if: :type_bunny
   validates :gender, presence: true
   validates :birthdate, presence: true
   validates :weight, presence: true # weight should be a positive number
@@ -12,6 +15,30 @@ class Pet < ApplicationRecord
   has_many :appointments, through: :user
 
   def types
-    # [Pet.all.pluck(:animal_type).uniq]
+    %w[Dog Cat Bunny]
+  end
+
+  def dog_breed
+    ['Boston Terrier', 'French Bulldog', 'Golden Retriever', 'Black Lab', 'Poodle', 'German Shepherd', 'Beagle', 'Chihuahua', 'Greyhound', 'English Bulldog', 'Pug', 'Siberian Husky', 'Boxer', 'Pointer', 'Yorkshire Terrier', 'Mutt']
+  end
+
+  def type_dog
+    self.breed = 'Dog'
+  end
+
+  def cat_breed
+    []
+  end
+
+  def type_cat
+    self.breed = 'Cat'
+  end
+
+  def bunny_breed
+    []
+  end
+
+  def type_bunny
+    self.breed = 'Bunny'
   end
 end
