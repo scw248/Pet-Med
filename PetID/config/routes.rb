b892d8d09ddfd95a443b68520bae88af4efb8d43
github username <github email address>
2019-04-11 09:48:49 -0500

    creates users#show route and made root

--- PetID/config/routes.rb
+++ PetID/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'users#show'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :vet_providers
   resources :appointments
