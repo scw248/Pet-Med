6cc5d2576e7c72712ff5466c98266b02b555707a
github username <github email address>
2019-04-14 12:29:29 -0500

    Updating validations in pet and appt models

--- PetID/app/models/appointment.rb
+++ PetID/app/models/appointment.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
-  # validates :validate_appt
+  validates :date_time, presence: true
+  validate :validate_appt
+  
   validates :reason_for_visit, presence: true
-  # validates :reason_for_visit, inclusion: { in: visit_reasons, presence: true }
+  validates :reason_for_visit, inclusion: { in: ['Dental', 'Medical Exam', 'Medication Refill', 'New Patient Visit', 'Surgery', 'Wellness Exam', 'Vaccinations'],
+  message: "%{value} is not a valid visit reason"}
+
   validates :vet_provider, presence: true
   belongs_to :user
   belongs_to :vet_provider
@@ -16,7 +20,7 @@ class Appointment < ApplicationRecord
   private
 
   def validate_appt
-      if date_time.present? && date_time > Date.today
+      if date_time < Date.today
           errors.add(:date_time, 'Appointments Must Be Booked At Least One Day in Advance')
       end
   end
--- PetID/app/models/pet.rb
+++ PetID/app/models/pet.rb
@@ -3,11 +3,19 @@
 class Pet < ApplicationRecord
   validates :name, presence: true
   validates :animal_type, presence: true
-  # validates :animal_type, inclusion: { in: self.allowed_types, presence: true }
+  validates :animal_type, inclusion: { in: %w[Dog Cat Bunny], message: "%{value} is not a valid animal type" }
+
   validates :breed, presence: true
-  # validates :breed, inclusion: { in: self.allowed_breeds, presence: true }, 
+  validates :breed, inclusion: { in:  ['Boston Terrier', 'French Bulldog', 'Golden Retriever', 'Black Lab', 'Poodle', 'German Shepherd', 'Beagle', '
+  Chihuahua', 'Greyhound', 'English Bulldog', 'Pug', 'Siberian Husky', 'Boxer', 'Pointer', 'Yorkshire Terrier', 
+  'Mutt', 'Russian Blue', 'Persian', 'British Shorthair', 'Scottish Fold', 'Siamese', 'Ragdoll', 'Maine Coon', 
+  'Munchkin', 'Sphynx', 'Abyssinian', 'Holland Lop', 'Rex Rabbit', 'Netherland Dwarf Rabbit', 
+  'Flemish Giant Rabbit', 'Mini Lop'], message: "%{value} is not a valid animal breed" }
+
   validates :gender, presence: true
-  # validates :gender, inclusion: { in: self.allowed_genders, presence: true }
+  validates :gender, inclusion: { in: %w[Male Female], message: "%{value} is not a valid gender" }
+
+  validates :birthdate, presence: true
   validate :validate_age
   validates :weight, presence: true
   validates :weight, numericality: { greater_than_or_equal_to: 0 }
@@ -36,7 +44,7 @@ class Pet < ApplicationRecord
   private
 
   def validate_age
-      if birthdate.present? && birthdate > 1.day.ago
+      if birthdate > 1.day.ago
           errors.add(:birthdate, 'Your Pet Should Be Over 1 Day Old.')
       end
   end
