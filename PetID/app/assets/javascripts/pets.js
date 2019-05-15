7d21d3df77e05f3c415b466e7cf9ed4efa18aac7
github username <github email address>
2019-05-15 18:39:42 -0500

    refactored respond_to in pets#index

--- PetID/app/assets/javascripts/pets.js
+++ PetID/app/assets/javascripts/pets.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 })
 
 class Pet {
-  constructor(id, name, animal_type, breed, gender, birthdate, weight, image) {
+  constructor(id, name, animal_type, breed, gender, birthdate, weight, user_id, image) {
     this.id = id
     this.name = name
     this.animal_type = animal_type
@@ -70,31 +70,32 @@ class Pet {
 }
 
 
-  // document.querySelector('.buttons').addEventListener('click', addPet)
-  // function addPet(e) {
-  //   e.preventDefault()
-  //   let data = {
-  //     'name': e.target.name.value,
-  //     'animal_type': e.target.animal_type.value,
-  //     'breed': e.target.breed.value,
-  //     'gender': e.target.gender.value,
-  //     'birthdate': e.target.birthdate.value,
-  //     'weight': e.target.weight.value,
-  //     'image': e.target.img.value
-  //   }
-  //   fetch('http://localhost:3000/users/${id}/pets', {
-  //     method: 'POST',
-  //     body: JSON.stringify(data),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   })
-  //     .then(res => res.json())
-  //     .then(pets => {
-  //       pets.forEach(pet => {
-  //         const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
-  //         new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
-  //         document.getElementById('?????').reset()
-  //       })
-  //     })
-  // }
+document.querySelector('.buttons').addEventListener('click', addPet)
+
+function addPet(e) {
+  e.preventDefault()
+  let data = {
+    'name': e.target.name.value,
+    'animal_type': e.target.animal_type.value,
+    'breed': e.target.breed.value,
+    'gender': e.target.gender.value,
+    'birthdate': e.target.birthdate.value,
+    'weight': e.target.weight.value,
+    'image': e.target.img.value
+  }
+  fetch(`http://localhost:3000/users/${id}/pets`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(pets => {
+      pets.forEach(pet => {
+        const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
+        new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
+        document.getElementById('new_pet.new_pet').reset()
+      })
+    })
+}
