ab9b36c698bad26997af7dd267f8aff53ff32fed
github username <github email address>
2019-05-16 11:20:26 -0500

    created index.js file and moved fetch and addPet method to it

--- PetID/app/assets/javascripts/pets.js
+++ PetID/app/assets/javascripts/pets.js
@@ -2,19 +2,6 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  let id = document.querySelector('#pet-container').dataset.id
-  fetch(`http://localhost:3000/users/${id}/pets.json`)
-    .then(res => res.json())
-    .then(pets => {
-      pets.forEach(pet => {
-        const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
-        new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
-      })
-    })
-})
-
 class Pet {
   constructor(id, name, animal_type, breed, gender, birthdate, weight, image) {
     this.id = id
@@ -71,36 +58,8 @@ class Pet {
           .removeChild(document.getElementById(id))
       })
   }
-
-  addPet(e) {
-    e.preventDefault()
-    let data = {
-      'name': e.target.name.value,
-      'animal_type': e.target.animal_type.value,
-      'breed': e.target.breed.value,
-      'gender': e.target.gender.value,
-      'birthdate': e.target.birthdate.value,
-      'weight': e.target.weight.value,
-      'image': e.target.image.value
-    }
-    fetch(`http://localhost:3000/users/${id}/pets`, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(pets => {
-        pets.forEach(pet => {
-          const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
-          new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
-          document.getElementById('form').reset()
-        })
-      })
-  }
 }
 
 
-document.getElementById('form').addEventListener("submit", Pet.addPet())
+
 
