31426c4d958e8104370b85547ddfdd804d6ecf89
github username <github email address>
2019-05-16 11:32:11 -0500

    removed index.js file and moved fetch and methods back to pets.js

--- PetID/app/assets/javascripts/pets.js
+++ PetID/app/assets/javascripts/pets.js
@@ -1,6 +1,17 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+window.addEventListener('DOMContentLoaded', (event) => {
+  let id = document.querySelector('#pet-container').dataset.id
+  fetch(`http://localhost:3000/users/${id}/pets.json`)
+    .then(res => res.json())
+    .then(pets => {
+      pets.forEach(pet => {
+        const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
+        new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
+      })
+    })
+})
 
 class Pet {
   constructor(id, name, animal_type, breed, gender, birthdate, weight, image) {
@@ -58,7 +69,39 @@ class Pet {
           .removeChild(document.getElementById(id))
       })
   }
+
+  addPet(e) {
+    e.preventDefault()
+    let data = {
+
+      'name': e.target.name.value,
+      'animal_type': e.target.animal_type.value,
+      'breed': e.target.breed.value,
+      'gender': e.target.gender.value,
+      'birthdate': e.target.birthdate.value,
+      'weight': e.target.weight.value,
+      'image': e.target.image.value
+    }
+    let id = document.querySelector('#pet-container').dataset.id
+    fetch(`http://localhost:3000/users/${id}/pets`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(pets => {
+        pets.forEach(pet => {
+          const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
+          new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
+          document.getElementById('form').reset()
+        })
+      })
+  }
 }
+
+document.querySelector('#form').addEventListener('submit', addPet())
 
 
 
