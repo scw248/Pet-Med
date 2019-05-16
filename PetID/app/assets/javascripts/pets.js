d75ffa0177e1d78d1439dc79bd4a414483b1d66c
github username <github email address>
2019-05-16 12:04:04 -0500

    updated pets#show for js and #show fetch in pets.js

--- PetID/app/assets/javascripts/pets.js
+++ PetID/app/assets/javascripts/pets.js
@@ -106,16 +106,17 @@ class Pet {
 
 document.querySelector('#form').addEventListener('submit', addPet())
 
+window.addEventListener('DOMContentLoaded', (event) => {
+  let id = document.querySelector('#pet-container').dataset.id
+  let pet_id = document.querySelector('#pet-container').dataset.pets.ids
+  fetch(`http://localhost:3000/users/${id}/pets/${pet_id}.json`)
+    .then(res => res.json())
+    .then(pets => {
+      pets.forEach(pet => {
+        const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
+        new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
+      })
+    })
+})
 
-// window.addEventListener('DOMContentLoaded', (event) => {
-//   let id = document.querySelector('#pet-container').dataset.id
-//   fetch(`http://localhost:3000/users/${id}/pets/${pet_id}.json`)
-//     .then(res => res.json())
-//     .then(pets => {
-//       pets.forEach(pet => {
-//         const { id, name, animal_type, breed, gender, birthdate, weight, image } = pet
-//         new Pet(id, name, animal_type, breed, gender, birthdate, weight, image)
-//       })
-//     })
-// })
 
